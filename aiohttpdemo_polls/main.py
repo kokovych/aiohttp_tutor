b2ded9e1bf273517cb37b643cb2d3bd602c2a2ec
Bohdan Kokovych <kokovych@gmail.com>
2019-07-04 23:14:28 +0300

    add template

--- aiohttpdemo_polls/main.py
+++ aiohttpdemo_polls/main.py
@@ -1,12 +1,17 @@
 # aiohttpdemo_polls/main.py
 from aiohttp import web
+import aiohttp_jinja2
+import jinja2
 
-from settings import config
+from settings import config, BASE_DIR
 from routes import setup_routes
 from db import close_pg, init_pg
 
+
 app = web.Application()
 app['config'] = config
+aiohttp_jinja2.setup(app,
+    loader=jinja2.FileSystemLoader(str(BASE_DIR / 'aiohttpdemo_polls' / 'templates')))
 setup_routes(app)
 app.on_startup.append(init_pg)
 app.on_cleanup.append(close_pg)
